a803c206ac0ac364124934718e3df2cfaab858f2
Ed Lomonaco <elomonaco127@gmail.com>
2021-09-13 20:31:51 -0400

    cleaned up example code

--- src/ExampleApp/ViewController.cs
+++ src/ExampleApp/ViewController.cs
@@ -18,9 +18,6 @@ namespace ExampleApp
 
             try
             {
-                // var urlString = "https://c.tenor.com/GZw_HxkxMl4AAAAC/betty-white-it-works.gif";
-                // var url = new NSUrl(urlString);
-                //var imageData = NSData.FromUrl(url);
 
                 // var path = NSBundle.MainBundle.PathForResource("success", "gif");
                 // var data = NSData.FromFile(path);
@@ -31,24 +28,12 @@ namespace ExampleApp
                     AnimatedImage = image
                 };
 
-                //View.AddSubview(aiv);
-
-                //ivGif = aiv;
-
                 ivGif.AnimatedImage = image;
-
-                //ivGif.SetImage(NSUrl.FromString("https://c.tenor.com/GZw_HxkxMl4AAAAC/betty-white-it-works.gif"), null, SDWebImageOptions.RefreshCached);
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
             }
-        }
-
-        public override void DidReceiveMemoryWarning()
-        {
-            base.DidReceiveMemoryWarning();
-            // Release any cached data, images, etc that aren't in use.
         }
     }
 }
